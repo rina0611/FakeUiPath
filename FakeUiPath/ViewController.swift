27a6c1174fa044471774bf0b5bbb2fd2f1ae2429
rina0611 <rina0611@korea.ac.kr>
2022-01-11 18:51:25 +0900

    Initial Commit

---
+++ FakeUiPath/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FakeUiPath
+//
+//  Created by 김혜구 on 2022/01/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
